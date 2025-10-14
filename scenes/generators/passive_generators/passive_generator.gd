9fd5c1cc591e4a9ac0a723a21a8dada280ec6094
mchang-orz <michaelchang@alumni.purdue.edu>
2025-10-13 21:14:34 -0700

    adding in passive gen, worked stopped at adding unlock button

---
+++ scenes/generators/passive_generators/passive_generator.gd
@@ -0,0 +1,40 @@
+class_name PassiveGenerator
+extends Node
+
+static var ref: PassiveGenerator
+
+func _init() -> void:
+	if not ref: ref = self
+	
+	else: queue_free()
+	
+#every second should generate one star
+var _cycle_duration : float = 1.0
+var _cycle_progress : float = 0.0
+var _passive_production : int = 1
+
+var _locked : bool = true
+
+func _ready() -> void:
+	Clock.ref.ticked.connect(_clock_tick)
+	
+	if Game.ref.data.upgrade_passive:
+		_locked = false
+	else:
+		_locked = true
+
+func _progress_cycles() -> void:
+	if _locked: return
+	
+	_cycle_progress += 1.0
+	
+	if _cycle_progress >= _cycle_duration:
+		_generate()
+
+func _generate() -> void:
+	_cycle_progress -= _cycle_duration
+	
+	ManagerStars.ref.create_stars(_passive_production)
+
+func _clock_tick() -> void:
+	_progress_cycles()
--- scripts/data.gd
+++ scripts/data.gd
@@ -1,3 +1,5 @@
 class_name Data
 
 var stars : int = 0
+
+var upgrade_passive : bool = false
